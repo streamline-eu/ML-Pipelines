314e3d940f1f1ac7b762ba96067e13d806476f57
Márton Balassi <balassi.marton@gmail.com>
2016-03-11 11:17:54 +0100

    [DO NOT MERGE] Stream-ml dummy examples

---
+++ flink-libraries/flink-stream-ml/src/main/scala/org/apache/flink/stream/ml/examples/SVMExample.scala
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.stream.ml.examples
+
+import org.apache.flink.api.scala._
+import org.apache.flink.ml.classification.SVM
+import org.apache.flink.ml.common.LabeledVector
+import org.apache.flink.ml.math.DenseVector
+import org.apache.flink.stream.ml.classification._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object SVMExample {
+  def main(args: Array[String]) {
+
+    val strEnv = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val stream = strEnv.fromElements(DenseVector(Array(0,0)))
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val input = env.fromElements(LabeledVector(0.0, DenseVector(Array(0,0))),
+      LabeledVector(1.0, DenseVector(Array(1,1))))
+
+    val model = SVM()
+
+    model.fit(input)
+
+    val result = model.predictStream(stream)
+
+    result.print()
+
+    strEnv.execute("Streaming SVM Classification")
+  }
+}
